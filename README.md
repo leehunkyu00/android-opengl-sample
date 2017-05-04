4f07127d829da6c74700365aa2c1beaf6dc5afcc
leehunkyu <leehunkyu00@gmail.com>
2017-05-04 22:07:55 +0900

    First commit with new Android project.

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # android-opengl-sample
 sample for opengl on android.
+
+# 환경
+mac os
+android studio
+android version : 4.0 ICS
+
---
+++ app/src/main/java/com/opengl/android/androidopenglsample/MainActivity.java
@@ -0,0 +1,13 @@
+package com.opengl.android.androidopenglsample;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
